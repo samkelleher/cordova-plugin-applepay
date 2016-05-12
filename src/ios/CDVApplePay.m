843034ed662f8227fc9832278185c266c0df4248
Sam Kelleher <sam@samkelleher.com>
2016-05-12 12:38:06 +0100

    Remove calls that breaks in iOS4

--- src/ios/CDVApplePay.m
+++ src/ios/CDVApplePay.m
@@ -4,25 +4,6 @@
 
 @synthesize paymentCallbackId;
 
-
-- (CDVPlugin*)initWithWebView:(UIWebView*)theWebView
-{
-    self = (CDVApplePay*)[super initWithWebView:(UIWebView*)theWebView];
-
-    return self;
-}
-
-- (void)dealloc
-{
-
-}
-
-- (void)onReset
-{
-
-}
-
-
 - (void)setMerchantId:(CDVInvokedUrlCommand*)command
 {
     merchantId = [command.arguments objectAtIndex:0];
@@ -33,42 +14,42 @@
 - (NSArray *)itemsFromArguments:(NSArray *)arguments
 {
     NSArray *itemDescriptions = [[arguments objectAtIndex:0] objectForKey:@"items"];
-    
+
     NSMutableArray *items = [[NSMutableArray alloc] init];
 
     for (NSDictionary *item in itemDescriptions) {
-        
+
         NSString *label = [item objectForKey:@"label"];
-        
+
         NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithDecimal:[[item objectForKey:@"amount"] decimalValue]];
-        
+
         PKPaymentSummaryItem *newItem = [PKPaymentSummaryItem summaryItemWithLabel:label amount:amount];
-        
+
         [items addObject:newItem];
     }
-    
+
     return items;
 }
 
 - (NSArray *)shippingMethodsFromArguments:(NSArray *)arguments
 {
     NSArray *shippingDescriptions = [[arguments objectAtIndex:0] objectForKey:@"shippingMethods"];
-    
+
     NSMutableArray *shippingMethods = [[NSMutableArray alloc] init];
-    
-    
+
+
      for (NSDictionary *desc in shippingDescriptions) {
-         
+
          NSString *identifier = [desc objectForKey:@"identifier"];
          NSString *detail = [desc objectForKey:@"detail"];
-         
+
          NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithDecimal:[[desc objectForKey:@"amount"] decimalValue]];
-         
+
          PKPaymentSummaryItem *newMethod = [self shippingMethodWithIdentifier:identifier detail:detail amount:amount];
-         
+
          [shippingMethods addObject:newMethod];
      }
-     
+
      return shippingMethods;
 }
 
@@ -81,35 +62,35 @@
         [self.commandDelegate sendPluginResult:result callbackId:self.paymentCallbackId];
         return;
     }
-    
+
     NSLog(@"ApplePay canMakePayments == %s", [PKPaymentAuthorizationViewController canMakePayments]? "true" : "false");
     if ([PKPaymentAuthorizationViewController canMakePayments] == NO) {
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"This device cannot make payments."];
         [self.commandDelegate sendPluginResult:result callbackId:self.paymentCallbackId];
         return;
     }
-    
+
     PKPaymentRequest *request = [PKPaymentRequest new];
-    
+
     // Must be configured in Apple Developer Member Center
     request.merchantIdentifier = merchantId;
-    
+
     [request setPaymentSummaryItems:[self itemsFromArguments:command.arguments]];
-    
+
     [request setShippingMethods:[self shippingMethodsFromArguments:command.arguments]];
-    
+
     // These appear to be the only 3 supported
     // Sorry, Discover Card
     request.supportedNetworks = @[PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa];
-    
+
     // What type of info you need (eg email, phone, address, etc);
     request.requiredBillingAddressFields = PKAddressFieldAll;
     request.requiredShippingAddressFields = PKAddressFieldPostalAddress;
-    
+
     // Which payment processing protocol the vendor supports
     // This value depends on the back end, looks like there are two possibilities
     request.merchantCapabilities = PKMerchantCapabilityEMV;// PKMerchantCapability3DS;
-    
+
     request.countryCode = @"US";
     request.currencyCode = @"USD";
 
@@ -118,13 +99,13 @@
     PKPaymentAuthorizationViewController *authVC = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
 
     authVC.delegate = self;
-    
+
     if (authVC == nil) {
         CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"PKPaymentAuthorizationViewController was nil."];
         [self.commandDelegate sendPluginResult:result callbackId:self.paymentCallbackId];
         return;
     }
-    
+
     [self.viewController presentViewController:authVC animated:YES completion:nil];
 }
 
@@ -155,7 +136,7 @@
     shippingMethod.detail = @"";
     shippingMethod.amount = amount;
     shippingMethod.label = detail;
-    
+
     return shippingMethod;
 }
 
