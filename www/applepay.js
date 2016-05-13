1e3ffc2fded842ff0081b86e15000ba0a0a15e9c
Sam Kelleher <sam@samkelleher.com>
2016-05-13 13:50:59 +0100

    Callbacks not required if using Promises.

--- www/applepay.js
+++ www/applepay.js
@@ -1,48 +1,53 @@
-
 var argscheck = require('cordova/argscheck'),
     utils = require('cordova/utils'),
     exec = require('cordova/exec');
 
+var executeCallback = function(callback, message) {
+    if (typeof callback === 'function') {
+        callback(message);
+    }
+}
+
 var ApplePay = {
 
     canMakePayments: function(successCallback, errorCallback) {
-      return new Promise(function(resolve, reject) {
-          exec(function(message) {
-            successCallback(message);
-            resolve(message);
-          }, function(message) {
-            errorCallback(message);
-            reject(message);
-          }, 'ApplePay', 'canMakePayments', []);
-      });
+        return new Promise(function(resolve, reject) {
+            exec(function(message) {
+                executeCallback(successCallback, message);
+                resolve(message);
+            }, function(message) {
+                executeCallback(errorCallback, message);
+                reject(message);
+            }, 'ApplePay', 'canMakePayments', []);
+        });
 
     },
 
     makePaymentRequest: function(order, successCallback, errorCallback) {
 
-      return new Promise(function(resolve, reject) {
-          exec(function(message) {
-            successCallback(message);
-            resolve(message);
-          }, function(message) {
-            errorCallback(message);
-            reject(message);
-          }, 'ApplePay', 'makePaymentRequest', [order]);
-      });
+        return new Promise(function(resolve, reject) {
+            exec(function(message) {
+                executeCallback(successCallback, message);
+                resolve(message);
+            }, function(message) {
+                executeCallback(errorCallback, message);
+                reject(message);
+            }, 'ApplePay', 'makePaymentRequest', [order]);
+        });
 
     },
 
     completeLastTransaction: function(status, successCallback, errorCallback) {
 
-      return new Promise(function(resolve, reject) {
-          exec(function(message) {
-            successCallback(message);
-            resolve(message);
-          }, function(message) {
-            errorCallback(message);
-            reject(message);
-          }, 'ApplePay', 'completeLastTransaction', [status]);
-      });
+        return new Promise(function(resolve, reject) {
+            exec(function(message) {
+                executeCallback(successCallback, message);
+                resolve(message);
+            }, function(message) {
+                executeCallback(errorCallback, message);
+                reject(message);
+            }, 'ApplePay', 'completeLastTransaction', [status]);
+        });
 
     }
 
