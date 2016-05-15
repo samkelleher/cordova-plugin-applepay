bde8ddd03fffc27775aeb66c309166877a9b11ea
Sam Kelleher <sam@samkelleher.com>
2016-05-15 15:04:49 +0100

    Add JSDoc to exposed methods.

--- www/applepay.js
+++ www/applepay.js
@@ -6,10 +6,16 @@ var executeCallback = function(callback, message) {
     if (typeof callback === 'function') {
         callback(message);
     }
-}
+};
 
 var ApplePay = {
 
+    /**
+     * Determines if the current device supports Apple Pay and has a supported card installed.
+     * @param {Function} [successCallback] - Optional success callback, recieves message object.
+     * @param {Function} [errorCallback] - Optional error callback, recieves message object.
+     * @returns {Promise}
+     */
     canMakePayments: function(successCallback, errorCallback) {
         return new Promise(function(resolve, reject) {
             exec(function(message) {
@@ -23,6 +29,12 @@ var ApplePay = {
 
     },
 
+    /**
+     * Opens the Apple Pay sheet and shows the order information.
+     * @param {Function} [successCallback] - Optional success callback, recieves message object.
+     * @param {Function} [errorCallback] - Optional error callback, recieves message object.
+     * @returns {Promise}
+     */
     makePaymentRequest: function(order, successCallback, errorCallback) {
 
         return new Promise(function(resolve, reject) {
@@ -37,6 +49,13 @@ var ApplePay = {
 
     },
 
+    /**
+     * While the Apple Pay sheet is still open, and the callback from the `makePaymentRequest` has completed,
+     * this call will pass the status to the sheet and close it if successfull.
+     * @param {Function} [successCallback] - Optional success callback, recieves message object.
+     * @param {Function} [errorCallback] - Optional error callback, recieves message object.
+     * @returns {Promise}
+     */
     completeLastTransaction: function(status, successCallback, errorCallback) {
 
         return new Promise(function(resolve, reject) {
