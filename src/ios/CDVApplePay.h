6b09905cc5929d178f6c7ea340010d3de0c21798
Andrew Crites <acrites@mobiquityinc.com>
2017-09-12 12:59:50 -0400

    Add shipping contact selection listener.
    * Provided methods to start / stop listening for shipping contact selection update.
    * Provided method to update items/shipping methods in response to contact update

--- src/ios/CDVApplePay.h
+++ src/ios/CDVApplePay.h
@@ -4,6 +4,7 @@
 #import <PassKit/PassKit.h>
 
 typedef void (^ARAuthorizationBlock)(PKPaymentAuthorizationStatus);
+typedef void (^ARListUpdateBlock)(PKPaymentAuthorizationStatus, NSArray<PKShippingMethod *>*, NSArray<PKPaymentSummaryItem *>*);
 
 @interface CDVApplePay : CDVPlugin <PKPaymentAuthorizationViewControllerDelegate>
 {
@@ -13,11 +14,21 @@ typedef void (^ARAuthorizationBlock)(PKPaymentAuthorizationStatus);
 
 @property (nonatomic, strong) ARAuthorizationBlock paymentAuthorizationBlock;
 
+@property (nonatomic, strong) ARListUpdateBlock updateItemsAndShippingMethodsBlock;
+
 @property (nonatomic, strong) NSString* paymentCallbackId;
 
+@property (nonatomic, strong) NSString* shippingContactSelectionListenerCallbackId;
+
+@property (nonatomic, strong) NSArray<PKShippingMethod *>* shippingMethods;
+
+@property (nonatomic, strong) NSArray<PKPaymentSummaryItem *>* summaryItems;
+
 - (void)makePaymentRequest:(CDVInvokedUrlCommand*)command;
+- (void)startListeningForShippingContactSelection:(CDVInvokedUrlCommand*)command;
+- (void)stopListeningForShippingContactSelection:(CDVInvokedUrlCommand*)command;
+- (void)updateItemsAndShippingMethods:(CDVInvokedUrlCommand*)command;
 - (void)canMakePayments:(CDVInvokedUrlCommand*)command;
 - (void)completeLastTransaction:(CDVInvokedUrlCommand*)command;
-
 
 @end
